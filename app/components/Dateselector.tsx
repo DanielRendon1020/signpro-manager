64469cce55b099cc38cb3f7456b57dc89c407d45
DanielRendon1020 <d.danny1020@gmail.com>
2023-09-18 15:27:01 -0700

    waiting of theme fix

--- app/components/Dateselector.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { Datepicker } from "flowbite-react"
-
-export default function Dateselector() {
-  return (
-    <Datepicker id="install-date" disabled />
-  )
-}
--- app/new_job/page.tsx
+++ app/new_job/page.tsx
@@ -1,6 +1,91 @@
+"use client"
 
 import { Metadata } from "next";
-import Dateselector from "../components/Dateselector";
+import { Datepicker } from 'flowbite-react';
+import type { CustomFlowbiteTheme } from 'flowbite-react';
+
+const customTheme: CustomFlowbiteTheme['datepicker'] = {
+  "root": {
+    "base": "relative"
+  },
+  "popup": {
+    "root": {
+      "base": "absolute top-10 z-50 block pt-2",
+      "inline": "relative top-0 z-auto",
+      "inner": "inline-block rounded-lg bg-white p-4 shadow-lg dark:bg-gray-700"
+    },
+    "header": {
+      "base": "",
+      "title": "px-2 py-3 text-center font-semibold text-gray-900 dark:text-white",
+      "selectors": {
+        "base": "flex justify-between mb-2",
+        "button": {
+          "base": "text-sm rounded-lg text-gray-900 dark:text-white bg-white dark:bg-gray-700 font-semibold py-2.5 px-5 hover:bg-gray-100 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-200 view-switch",
+          "prev": "",
+          "next": "",
+          "view": ""
+        }
+      }
+    },
+    "view": {
+      "base": "p-1"
+    },
+    "footer": {
+      "base": "flex mt-2 space-x-2",
+      "button": {
+        "base": "w-full rounded-lg px-5 py-2 text-center text-sm font-medium focus:ring-4 focus:ring-orange-300",
+        "today": "bg-orange-500 text-white hover:bg-orange-600 dark:bg-orange-400 dark:hover:bg-orange-500",
+        "clear": "border border-gray-300 bg-white text-gray-900 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600"
+      }
+    }
+  },
+  "views": {
+    "days": {
+      "header": {
+        "base": "grid grid-cols-7 mb-1",
+        "title": "dow h-6 text-center text-sm font-medium leading-6 text-gray-500 dark:text-gray-400"
+      },
+      "items": {
+        "base": "grid w-64 grid-cols-7",
+        "item": {
+          "base": "block flex-1 cursor-pointer rounded-lg border-0 text-center text-sm font-semibold leading-9 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600 ",
+          "selected": "bg-orange-400 text-white hover:bg-orange-600",
+          "disabled": "text-gray-500"
+        }
+      }
+    },
+    "months": {
+      "items": {
+        "base": "grid w-64 grid-cols-4",
+        "item": {
+          "base": "block flex-1 cursor-pointer rounded-lg border-0 text-center text-sm font-semibold leading-9 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600",
+          "selected": "bg-orange-400 text-white hover:bg-orange-600",
+          "disabled": "text-gray-500"
+        }
+      }
+    },
+    "years": {
+      "items": {
+        "base": "grid w-64 grid-cols-4",
+        "item": {
+          "base": "block flex-1 cursor-pointer rounded-lg border-0 text-center text-sm font-semibold leading-9 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600 text-gray-900",
+          "selected": "bg-orange-400 text-white hover:bg-orange-600",
+          "disabled": "text-gray-500"
+        }
+      }
+    },
+    "decades": {
+      "items": {
+        "base": "grid w-64 grid-cols-4",
+        "item": {
+          "base": "block flex-1 cursor-pointer rounded-lg border-0 text-center text-sm font-semibold leading-9  hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600 text-gray-900",
+          "selected": "bg-orange-400 text-white hover:bg-orange-600",
+          "disabled": "text-gray-500"
+        }
+      }
+    }
+  }
+};
 
 export const metadata: Metadata = {
   title: "New Job",
@@ -112,8 +197,7 @@ export default function NewJob() {
             Install Date
           </span>
         </label>
-        <Dateselector />
-
+        <Datepicker theme={customTheme} inline className="flex justify-center"/>
         <button
           type="submit"
           className="text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"

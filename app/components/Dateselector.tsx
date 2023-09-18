bab5421df6d3c3fda20bb289449c83e6910f471d
DanielRendon1020 <d.danny1020@gmail.com>
2023-09-18 13:40:17 -0700

    new job form and project table updated

---
+++ app/components/Dateselector.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { Datepicker } from "flowbite-react"
+
+export default function Dateselector() {
+  return (
+    <Datepicker id="install-date" disabled />
+  )
+}
--- app/new_job/page.tsx
+++ app/new_job/page.tsx
@@ -1,15 +1,6 @@
-"use client";
 
 import { Metadata } from "next";
-import {
-  Button,
-  Checkbox,
-  Label,
-  Select,
-  Textarea,
-  TextInput,
-  Datepicker,
-} from "flowbite-react";
+import Dateselector from "../components/Dateselector";
 
 export const metadata: Metadata = {
   title: "New Job",
@@ -17,65 +8,119 @@ export const metadata: Metadata = {
 
 export default function NewJob() {
   return (
-    <form className="flex max-w-xl flex-col gap-4 mx-auto mt-10">
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="project-name">
-            <span className="text-orange-500">*</span>Project Name
-          </Label>
+    <>
+      <form className="flex max-w-xl flex-col gap-4 mx-auto my-10">
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            type="text"
+            name="project-name"
+            id="project-name"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-orange-500 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="project-name"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Project Name
+          </label>
         </div>
-        <TextInput id="project-name" required type="text" />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="address">
-            <span className="text-orange-500">*</span>Address
-          </Label>
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            type="text"
+            name="address"
+            id="address"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-orange-500 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="address"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Address
+          </label>
         </div>
-        <TextInput id="address" required type="text" />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="client-name">
-            <span className="text-orange-500">*</span>Client's Name
-          </Label>
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            type="text"
+            name="client-name"
+            id="client-name"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-orange-500 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="client-name"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Client's Name
+          </label>
         </div>
-        <TextInput id="client-name" required type="text" />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="client-email">
-            <span className="text-orange-500">*</span>Client's Email
-          </Label>
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            type="email"
+            name="client-email"
+            id="client-email"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-orange-500 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="client-email"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Client's Email
+          </label>
         </div>
-        <TextInput id="client-email" required type="email" />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="client-number" value="Client's Number" />
+        <div className="relative z-0 w-full mb-6 group">
+          <input
+            type="tel"
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+            name="client-tel"
+            id="client-tel"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-orange-500 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="client-tel"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-orange-600 peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Client's Number (123-456-7890)
+          </label>
         </div>
-        <TextInput id="client-number" placeholder="000-000-0000" type="tel" />
-      </div>
-      <div className="flex items-center gap-2">
-        <Checkbox id="design-file" />
-        <Label htmlFor="design-file" value="Design File" />
-      </div>
-      <div className="flex items-center gap-2">
-        <Checkbox id="invoice" />
-        <Label htmlFor="invoice">Invoice</Label>
-      </div>
-      <div className="flex items-center gap-2">
-        <Checkbox id="install" />
-        <Label htmlFor="install" value="Install Date" />
-      </div>
-      <Datepicker id="install-date" disabled />
-      <div>
-        <Label htmlFor="notes" value="Notes" />
-        <Textarea id="notes" />
-      </div>
-      <Button type="submit" pill>
-        Submit
-      </Button>
-    </form>
+        <label className="relative inline-flex items-center mb-4 cursor-pointer">
+          <input type="checkbox" value="" className="sr-only peer" />
+          <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-700 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-500"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Design File
+          </span>
+        </label>
+        <label className="relative inline-flex items-center mb-4 cursor-pointer">
+          <input type="checkbox" value="" className="sr-only peer" />
+          <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-700 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-500"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Invoice
+          </span>
+        </label>
+        <label className="relative inline-flex items-center mb-4 cursor-pointer">
+          <input type="checkbox" value="" className="sr-only peer" />
+          <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-700 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-500"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Install Date
+          </span>
+        </label>
+        <Dateselector />
+
+        <button
+          type="submit"
+          className="text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+        >
+          Submit
+        </button>
+      </form>
+    </>
   );
 }
